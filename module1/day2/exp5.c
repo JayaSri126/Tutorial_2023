e7d8561ef7446221f9789a28c699486408905899
JayaSri126 <136588000+JayaSri126@users.noreply.github.com>
2023-06-16 13:31:44 +0000

    add day two solution of l1 activity

--- module1/day2/exp5.c
+++ module1/day2/exp5.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
-int count_bits(int array[], int size) {
+int count_bits(int *array, int size) {
   int total_bits = 0;
   for (int i = 0; i < size; i++) {
-    int num = array[i];
-    int count = 0;
-    while (num != 0) {
-      if (num & 1) {
-        count++;
+    for (int j = 0; j < 32; j++) {
+      if ((array[i] & (1 << j)) != 0) {
+        total_bits++;
       }
-      num >>= 1;
     }
-    total_bits += count;
   }
   return total_bits;
 }
@@ -19,9 +15,7 @@ int count_bits(int array[], int size) {
 int main() {
   int a[] = {0x1, 0xF4, 0x10001};
   int size = sizeof(a) / sizeof(a[0]);
-
-  int result = count_bits(a, size);
-  printf("Total bits: %d\n", result);
-
+  int total_bits = count_bits(a, size);
+  printf("The total number of bits set is %d\n", total_bits);
   return 0;
 }
